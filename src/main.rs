c41c24a9896604507b5e3a3d0c43661823750111
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-24 22:51:29 +0400

    3.2. Data Types
    access array elements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-   let _a: [i32; 5] = [1, 2, 3, 4, 5];
+   let a: [i32; 5] = [1, 2, 3, 4, 5];
    let _months: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
-
+   let _first = a[0];
+   let _second = a[1];
 }
