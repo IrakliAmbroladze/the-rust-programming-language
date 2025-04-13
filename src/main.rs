9bcfa65b0878dcb1d0652422d1c9b2ab21d121da
Irakli <ambroladzeirakli@gmail.com>
2025-04-13 08:27:05 +0400

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-   let mut number = 3;
-   while number != 0 {
-       println!("{number}!");
-       number -= 1;
+   let a = [10, 20, 30, 40, 50];
+   for element in a {
+      println!("the  value is: {element}")
    }
-   
-   println!("LIFTOFF!!!")
 }
