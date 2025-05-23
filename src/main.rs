a58b723ea39b7dcc575a6bef5f807fc8f0d7025a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-05-23 07:39:14 +0400

    4.1 Scope and Assignment. drop automatically when reassigned variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+//Scope and Assignment
+
 fn main() {
-   let mut s: String = String::from("hello");
-   s.push_str( ", world!");
-   println!("{s}")
+let mut s = String::from("hello");
+s = String::from("ahoy");
+println!("{s}, world");
+   
 }
