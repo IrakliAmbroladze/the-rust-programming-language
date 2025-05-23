d6e8e0b91eea0ef59236b3bc45ac8a1be64aa1b8
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-05-23 08:05:46 +0400

    /4.1 Stack-Only Data: Copy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-//Variables and Data Interacting with Clone
+//4.1 Stack-Only Data: Copy
 
 fn main() {
-   let s1 = String::from("hello");
-   let s2 = s1.clone();
-   println!("s1 = {s1}, s2 = {s2}");
-   println!("{}",true);
-   println!("{}",5==5);
-   println!("s1==s2: {}", s1==s2);
+   let x =  5;
+   let y = x;
+   println!("x={x}, y={y}");
+//    Types such as integers that have a known size at a compile time
+//are stored entirely on the stack. So there is no need for deep copy
+//with clone
 }
