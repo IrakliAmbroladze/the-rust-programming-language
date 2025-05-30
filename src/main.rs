c8ba0f6afd364031ea78b014efea46cd10b750fb
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-05-30 05:02:51 +0400

    /4.1 Return Values and Scope: Listing 4-4: Transferring ownership of return values

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-//4.1 Ownership and Functions
+//4.1 Return Values and Scope
 
 fn main() {
-   let s = String::from("hello");
-   println!("{}", s);
-   takes_ownership(s);
-
-   let x = 5;
-   makes_copy(x);
-
-   println!("{}", x);
+   let s1 = gives_ownership();
+   let s2 = String::from("hello");
+   let s3 = takes_and_gives_back(s2);
+   let s4 = gives_ownership();
+   let s5 = takes_and_gives_back(s3);
+   println!("{}", s1);
+   println!("{}", s4);
+   println!("{}", s5);
 }
 
-fn takes_ownership(_some_string: String) {
-    
+fn gives_ownership()->String{
+   let some_string= String::from("yours");
+   
+   some_string
 }
-fn makes_copy(_some_integer: i32) {
-    
+
+fn takes_and_gives_back(a_string: String)->String{
+   a_string
 }
