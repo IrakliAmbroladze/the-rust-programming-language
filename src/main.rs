23b03015fdcbeb453feeaf9da895ba9e65b55170
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-04 17:36:33 +0400

    my first fn vlen

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 fn main() {
-    let x: Box<i32> = Box::new(-1);
-    let x_abs1 = i32::abs(*x);
-    let x_abs2 = x.abs();
-    assert_eq!(x_abs1, x_abs2);
-
-    let r: &Box<i32> = &x;
-    let r_abs1 = i32::abs(**r);
-    let r_abs2 = r.abs();
-    assert_eq!(r_abs1, r_abs2);
-
-    let s = String::from("Hello");
-    let s_len1 = str::len(&s);
-    let s_len2 = s.len();
-    assert_eq!(s_len1, s_len2);
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    v.push(4);
+    println!("{}", v.len());
 }
