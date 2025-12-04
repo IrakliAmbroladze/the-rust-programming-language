c6f21f44dbb4e8babd060a13028e0268e4ae7a98
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-04 15:35:35 +0400

    using dereference in different ways

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    let m1 = String::from("Hello");
-    let m2 = String::from("world");
-    greet(&m1, &m2);
-    let s = format!("{} {}", m1, m2);
-}
+    let mut x: Box<i32> = Box::new(1);
+    let a: i32 = *x;
+    *x += 1;
 
-fn greet(g1: &String, g2: &String) {
-    println!("{} {}!", g1, g2);
+    let r1: &Box<i32> = &x;
+    let b: i32 = **r1;
+
+    let r2: &i32 = &*x;
+    let c: i32 = *r2;
 }
