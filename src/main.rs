af1e66803d3d3bedff1834a280358e84c206905f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-11 17:29:01 +0400

    create mutable reference

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
     let mut v: Vec<i32> = vec![1, 2, 3];
-    v.push(4);
-    println!("{}", v.len());
+    let num: &mut i32 = &mut v[2];
+    *num += 1;
+    println!("Third element is {}", *num);
+    println!("Vector is now {:?}", v);
+    println!("Vector is now {:?}", v);
+    println!("Vector is now {}", v[2]);
+
+    v[1] += 3;
+    println!("Vector is now {}", v[1]);
+    println!("Vector is now {:?}", v);
 }
