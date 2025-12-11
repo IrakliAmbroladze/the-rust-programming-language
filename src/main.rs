2bef5a12b1b85cd2d0e4ea2fe51f0a09f92cf4ca
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-11 17:34:13 +0400

    downgrade to read0only reference

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 fn main() {
     let mut v: Vec<i32> = vec![1, 2, 3];
     let num: &mut i32 = &mut v[2];
-    *num += 1;
-    println!("Third element is {}", *num);
-    println!("Vector is now {:?}", v);
-    println!("Vector is now {:?}", v);
-    println!("Vector is now {}", v[2]);
-
-    v[1] += 3;
-    println!("Vector is now {}", v[1]);
-    println!("Vector is now {:?}", v);
+    let num2: &i32 = &*num;
+    println!("{}{}", *num, *num2);
 }
