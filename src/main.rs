1a13c970e9a4438bc539c793fd9f3c08f4417a4b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-29 22:57:17 +0400

    debug prind derive Debug

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
-#[allow(dead_code)]
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-#[allow(unused_variables)]
 fn main() {
-    let rec1: Rectangle = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    let width1: u32 = 30;
-    let height1: u32 = 50;
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rec1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect1 is {rect1:?}");
 }
