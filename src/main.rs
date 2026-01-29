d9fa5103121dc812f43b148bdc74aa344d0ea536
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-29 17:20:55 +0400

    feat: calculate square using tuple

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 #[allow(unused_variables)]
 fn main() {
+    let rec1: (u32, u32) = (30, 50);
     let width1: u32 = 30;
     let height1: u32 = 50;
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
-    );
+    println!("The area of the rectangle is {} square pixels.", area(rec1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
