c0aaf252393fbd3765ec2c9a20637c528c2ec6de
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-30 22:38:28 +0400

    6. Defining an Enum

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
-#[derive(Debug, Copy, Clone)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn can_hold(&self, rectangle: &Rectangle) -> bool {
-        self.width > rectangle.width && self.height > rectangle.height
-    }
-    fn set_width(&mut self, width: u32) {
-        self.width = width;
-    }
-    fn max(self, other: Self) -> Self {
-        Rectangle {
-            width: self.width.max(other.width),
-            height: self.height.max(other.height),
-        }
-    }
-    fn set_to_max(&mut self, other: Rectangle) {
-        let max = self.max(other);
-        *self = max;
-    }
-}
-
-#[allow(unused_variables)]
 fn main() {
-    let rect = Rectangle {
-        width: 0,
-        height: 0,
-    };
-    println!("{}", rect.area());
-    let other_rect = Rectangle {
-        width: 1,
-        height: 1,
-    };
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    impl Message {
+        fn call(&self) {}
+    }
 
-    let max_rect: Rectangle = rect.max(other_rect);
-    println!("max_rect is {max_rect:#?}");
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
