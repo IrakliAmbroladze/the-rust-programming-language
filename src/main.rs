3f4f84bf7a980c25db617bb4b376e1b3f2a59d02
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-05 14:31:22 +0400

    executed program that uses mut reference on Vec and dereferences

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let v = vec![String::from("Hello ")];
-    let mut s = v[0].clone();
-    s.push_str("world");
-    println!("{s}");
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    let mut v2: Vec<&mut i32> = Vec::new();
+    for i in &mut v {
+        v2.push(i);
+    }
+    *v2[0] = 5;
+
+    let a = *v2[0];
+    let b = v[0];
+    println!("{a} {b}");
 }
