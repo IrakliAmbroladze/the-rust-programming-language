30af14fd5f41daa3a664ef38e9b61791c8f034df
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-27 23:55:07 +0400

    feat: implement main function for giving a promotion free t-shirt

--- src/main.rs
+++ src/main.rs
@@ -35,5 +35,16 @@ fn main() {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
     };
     let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
     let user_pref2: Option<ShirtColor> = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 }
