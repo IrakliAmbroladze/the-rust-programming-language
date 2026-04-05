5428bf64fb7322e93203ddc83bfb1351867caa77
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-05 11:04:33 +0400

    Implementations of the public methods add, remove, and average on AveragedCollection

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 #![allow(unused)]
-struct Point<T> {
-    x: T,
-    y: T,
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
     }
 }
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
-    println!("p.x = {}", p.x());
+    println!("hello");
 }
