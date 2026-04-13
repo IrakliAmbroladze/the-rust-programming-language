7971aeee3901f8c172b2aee0f400b788270914a0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-13 10:48:38 +0400

    feat: spawn new thread from main

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
-#![allow(unused)]
-pub struct AveragedCollection {
-    list: Vec<i32>,
-    average: f64,
-}
-
-impl AveragedCollection {
-    pub fn add(&mut self, value: i32) {
-        self.list.push(value);
-        self.update_average();
-    }
-
-    pub fn remove(&mut self) -> Option<i32> {
-        let result = self.list.pop();
-        match result {
-            Some(value) => {
-                self.update_average();
-                Some(value)
-            }
-            None => None,
-        }
-    }
-
-    pub fn average(&self) -> f64 {
-        self.average
-    }
-
-    fn update_average(&mut self) {
-        let total: i32 = self.list.iter().sum();
-        self.average = total as f64 / self.list.len() as f64;
-    }
-}
-
+use std::thread;
+use std::time::Duration;
 fn main() {
-    println!("hello");
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("Hello {i} from thread");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..5 {
+        println!("Hello {i} from main");
+        thread::sleep(Duration::from_millis(1));
+    }
 }
