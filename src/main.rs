5eb9f3dcf1c86a7d514ea6d1d7dbbaad8196c5f3
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-13 17:02:39 +0400

    send message from spawned thread via channel

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::sync::mpsc;
 use std::thread;
+
 fn main() {
-    let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || println!("Here's a vector: {v:#?}"));
-    handle.join().unwrap();
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+    let received = rx.recv().unwrap();
+    println!("Got {received}")
 }
