2e56e7976a45f7f87d3e0bed9c26882b6b35345d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-17 07:22:14 +0400

    create loop in spawned task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 use ::std::time::Duration;
 
-fn main() {}
+fn main() {
+    trpl::run(async {
+        trpl::spawn_task(async {
+            for i in 1..5 {
+                println!("Hello form {i} first loop");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        })
+    });
+}
