93ab4de0b51f7807d96d4ae3b60edbe09373159a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-29 16:32:54 +0400

    Listing 18-11: Code that demonstrates the desired behavior we want our blog crate to have

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-use ::std::time::Duration;
+use blog::Post;
 
 fn main() {
-    trpl::run(async {
-        let fut1 = async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
-        let fut2 = async {
-            for i in 1..5 {
-                println!("hi number {i} from the second task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
+    let mut post = Post::new();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
 
-        trpl::join(fut1, fut2).await;
-    });
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
