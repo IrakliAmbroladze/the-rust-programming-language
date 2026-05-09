67806a13b91c2bbf48c57c7868207eafbf317b63
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-05-09 11:25:15 +0400

    Listing 19-3: Mixing if let, else if, else if let, and else

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 fn main() {
-    let (x, y) = (1, 2, 3);
-    println!("{x} {y} {z}")
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
 }
