da7214e10f727d7612dc918ae9e82e12101c6783
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-05-16 22:31:15 +0400

    feat(Advanced Traits): Two traits are defined to have a fly method and are implemented on the Human type

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
-use std::ops::Add;
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Point {
-    x: i32,
-    y: i32,
+trait Pilot {
+    fn fly(&self);
+}
+trait Wizard {
+    fn fly(&self);
 }
 
-impl Add for Point {
-    type Output = Point;
-    fn add(self, other: Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+struct Human;
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*")
     }
 }
 fn main() {
-    assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 },
-    )
+    Human.fly();
 }
