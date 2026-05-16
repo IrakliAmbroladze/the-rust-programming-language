d7dd5fb63508b526268fcd93ec58c118d62a3b11
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-05-16 22:51:45 +0400

    feat(Advanced Traits): Using fully qualified syntax to specify that we want to call the baby_name function from the Animal trait as implemented on Dog

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,5 @@ impl Animal for Dog {
     }
 }
 fn main() {
-    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
