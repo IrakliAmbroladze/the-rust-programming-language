19c0b1ad73d69129d08672f77a8b5017c8b86d89
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-05-16 22:48:52 +0400

    feat(Advanced Traits): Listing 20-20: A trait with an associated function and a type with an associated function of the same name that also implements the trait

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
-trait Pilot {
-    fn fly(&self);
+trait Animal {
+    fn baby_name() -> String;
 }
-trait Wizard {
-    fn fly(&self);
-}
-
-impl Pilot for Human {
-    fn fly(&self) {
-        println!("This is your captain speaking.");
+struct Dog;
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
     }
 }
-
-impl Wizard for Human {
-    fn fly(&self) {
-        println!("Up!");
-    }
-}
-struct Human;
-impl Human {
-    fn fly(&self) {
-        println!("*waving arms furiously*")
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
 fn main() {
-    let person = Human;
-    Pilot::fly(&person);
-    Wizard::fly(&person);
-    person.fly();
+    println!("A baby dog is called a {}", Dog::baby_name());
 }
