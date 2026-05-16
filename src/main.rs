8c39b0f31d66e5d3a6afc39547467ae66efb3dac
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-05-17 01:05:24 +0400

    feat(Advanced Traits): add Display trait impl for Point

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::fmt;
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+impl OutlinePrint for Point {}
 trait OutlinePrint: fmt::Display {
     fn outline_print(&self) {
         let output = self.to_string();
         let len = output.len();
         println!("{}", "*".repeat(len + 4));
-        println!("*{}*", "*".repeat(len + 2));
+        println!("*{}*", " ".repeat(len + 2));
         println!("* {output} *");
-        println!("*{}*", "*".repeat(len + 2));
+        println!("*{}*", " ".repeat(len + 2));
         println!("{}", "*".repeat(len + 4));
     }
 }
-fn main() {}
+fn main() {
+    let p = Point { x: 1, y: 3 };
+    p.outline_print();
+}
